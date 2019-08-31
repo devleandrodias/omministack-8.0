d7ab78ca83d3a4aa6d2115622bd6255ea05e0540
devleandrodias <leandro.dias_profissional@outlook.com.br>
2019-08-31 11:02:27 -0300

    modulo-05

---
+++ modulo-05/babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: ["@babel/preset-env", "@babel/preset-react"]
+};
+
+//preset-env (Altera o javascript par um que o navegador entenda)
+//preset-react (Alterar as coisas especificas do react tipo JSX para que o navegador entendenda)
