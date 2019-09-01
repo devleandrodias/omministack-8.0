9f82630a80837e319e5df0b780f7ca06393f5905
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-09-01 13:23:51 -0300

    modulo-06

---
+++ modulo-06/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn', { extension: ['.jsx', '.js'] }
+    ],
+    'import/prefer-default-export': 'off'
+  },
+};
--- modulo-06/src/App.js
+++ modulo-06/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
   return (
@@ -8,4 +8,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
