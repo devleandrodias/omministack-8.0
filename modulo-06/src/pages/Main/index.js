b7e3a6b158fb12b5559c406dd6e36582e0f5a35e
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-09-01 15:40:02 -0300

    modulo-06

--- modulo-06/src/pages/Main/index.js
+++ modulo-06/src/pages/Main/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Title } from './style';
+import { FaGithub, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './style';
 
 function Main() {
-  return <Title>Main</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithub />
+        Repositório
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+        <SubmitButton disabled>
+          <FaPlus />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
