8f77382153be4f23793eed52372a493abc0c47c9
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-09-01 14:47:02 -0300

    modulo-06

--- modulo-06/src/App.js
+++ modulo-06/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Route from './routes';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World, React!</h1>
+      <Route />
     </div>
   );
 }
