d9ea7b0256537ed203aada4b6372ac07eafa7111
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-09-01 16:56:05 -0300

    modulo-06

--- modulo-06/src/pages/Main/index.js
+++ modulo-06/src/pages/Main/index.js
@@ -1,23 +1,74 @@
-import React from 'react';
-import { FaGithub, FaPlus } from 'react-icons/fa';
-import { Container, Form, SubmitButton } from './style';
+import React, { Component } from 'react';
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Container, Form, SubmitButton, List } from './style';
+import api from '../../services/api';
 
-function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithub />
-        Repositório
-      </h1>
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar repositório" />
-        <SubmitButton disabled>
-          <FaPlus />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+    this.state = {
+      newRepo: '',
+      repositories: [],
+      loading: false,
+    };
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    const { newRepo, repositories } = this.state;
+
+    this.setState({ loading: true });
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, loading, repositories } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithub />
+          Repositório
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton loading={loading}>
+            {loading ? <FaSpinner /> : <FaPlus />}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map(repo => (
+            <li key={repo.name}>
+              <span>{repo.name}</span>
+              <a href="# ">Detalhes</a>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
 }
-
-export default Main;
--- modulo-06/src/pages/Main/style.js
+++ modulo-06/src/pages/Main/style.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 // color: ${props => (props.error ? 'red' : '#7159c1')};
 
@@ -36,9 +36,20 @@ export const Form = styled.form`
   }
 `;
 
-export const SubmitButton = styled.button.attrs({
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg)
+  }
+
+  to {
+    transform: rotate(360deg)
+  }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
   type: 'submit',
-})`
+  disabled: props.loading,
+}))`
   background: #7159c1;
   border: 0;
   padding: 0 15px;
@@ -51,7 +62,42 @@ export const SubmitButton = styled.button.attrs({
   justify-content: center;
   align-items: center;
 
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+
   svg {
     margin: 0;
+  }
+
+  ${props =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${rotate} 3s linear infinite;
+      }
+    `}
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #7159c1;
+      text-decoration: none;
+    }
   }
 `;
