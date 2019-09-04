72fda031582dc7d32b700a53f9b207e32bce4f6d
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-09-03 21:01:26 -0300

    modulo-07

--- modulo-07/src/pages/Cart/index.js
+++ modulo-07/src/pages/Cart/index.js
@@ -1,5 +1,67 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Produto</th>
+            <th>Quantidade</th>
+            <th>SubTotal</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-de-caminhada-leve-confortavel/06/E74-0492-006/E74-0492-006_zoom1.jpg"
+                alt="Tenis"
+              />
+            </td>
+            <td>
+              <strong>Tenis muito legal</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar compra</button>
+        <Total>
+          <span>Total</span>
+          <strong>R$129,00</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
