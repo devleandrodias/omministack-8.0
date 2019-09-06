a3ee4e40db8511bde6783735d392ba56cb74f919
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-09-05 21:42:55 -0300

    modulo-07

--- modulo-07/src/store/modules/cart/sagas.js
+++ modulo-07/src/store/modules/cart/sagas.js
@@ -13,13 +13,24 @@ import { formatPrice } from '../../../util/format';
 function* addToCart({ id }) {
   const productExist = yield select(state => state.cart.find(p => p.id === id));
 
-  const response = yield call(api.get, `/products/${id}`);
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+
+  const currentAmount = productExist ? productExist.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    console.tron.warn('erro');
+    return;
+  }
 
   if (productExist) {
-    const amount = productExist.amount + 1;
-
     yield put(updateAmount(id, amount));
   } else {
+    const response = yield call(api.get, `/products/${id}`);
+
     const data = {
       ...response.data,
       amount: 1,
