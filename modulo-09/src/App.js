08e8ab09c543be1224533593eb173f170cb36bc8
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-09-07 11:55:51 -0300

    modulo-09

--- modulo-09/src/App.js
+++ modulo-09/src/App.js
@@ -1,5 +1,33 @@
-import React from 'react';
+/* O use state retorn duas funçoes a primeira
+é o estado em si e a segunda propriedade usada para alterar o estado */
+import React, { useState } from 'react';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+function App() {
+  const [techs, setTechs] = useState(['ReactJs', 'ExpressJs', 'NodeJs']);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
+
+export default App;
