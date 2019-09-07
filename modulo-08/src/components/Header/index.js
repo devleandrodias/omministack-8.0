f65f3841bf0027fe2ba70cf0872fe9459bfa6de1
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-09-07 13:46:56 -0300

    modulo-08

--- modulo-08/src/components/Header/index.js
+++ modulo-08/src/components/Header/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import Logo from '../../assets/images/logo.svg';
 import { Container, Cart } from './styles';
 
-function Header({ cartSize }) {
+// function Header({ cartSize })
+
+export default function Header() {
+  const cartSize = useSelector(state => state.cart.length);
+
   return (
     <Container>
       <Link to="/">
@@ -24,6 +29,6 @@ function Header({ cartSize }) {
   );
 }
 
-export default connect(state => ({
-  cartSize: state.cart.length,
-}))(Header);
+// export default connect(state => ({
+//   cartSize: state.cart.length,
+// }))(Header);
--- modulo-08/src/pages/Home/index.js
+++ modulo-08/src/pages/Home/index.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
+// import { bindActionCreators } from 'redux';
 
-import * as CartActions from '../../store/modules/cart/actions';
+import { addToCartRequest } from '../../store/modules/cart/actions';
 
 import { formatPrice } from '../../util/format';
 import api from '../../service/api';
 import { ProductList } from './styles';
 
-function Home({ amount, addToCartRequest }) {
+export default function Home() {
   const [products, setProducts] = useState([]);
+
+  const amount = useSelector(state =>
+    state.cart.reduce((sumAmount, product) => {
+      sumAmount[product.id] = product.amount;
+
+      return sumAmount;
+    }, {})
+  );
+
   // constructor(props) {
   //   super(props);
 
@@ -18,6 +27,8 @@ function Home({ amount, addToCartRequest }) {
   //     products: [],
   //   };
   // }
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     async function loadProducts() {
@@ -48,7 +59,7 @@ function Home({ amount, addToCartRequest }) {
   function handleAddProduct(id) {
     // const { addToCartRequest } = this.props;
 
-    addToCartRequest(id);
+    dispatch(addToCartRequest(id));
   }
 
   // const { products } = this.state;
@@ -75,18 +86,18 @@ function Home({ amount, addToCartRequest }) {
   );
 }
 
-const mapStateToProps = state => ({
-  amount: state.cart.reduce((amount, product) => {
-    amount[product.id] = product.amount;
+// const mapStateToProps = state => ({
+//   amount: state.cart.reduce((amount, product) => {
+//     amount[product.id] = product.amount;
 
-    return amount;
-  }, {}),
-});
+//     return amount;
+//   }, {}),
+// });
 
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(CartActions, dispatch);
+// const mapDispatchToProps = dispatch =>
+//   bindActionCreators(CartActions, dispatch);
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home);
+// export default connect(
+//   null,
+//   mapDispatchToProps
+// )(Home);
