ee1c3954023b001a56f393e23d2df0355303d247
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-09-11 06:48:30 -0300

    modulo-03

--- modulo-03/src/routes/index.js
+++ modulo-03/src/routes/index.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from '../app/models/User';
+import UserController from '../app/controllers/UserController';
 
 const routes = Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Leandro Dias',
-    email: 'leandro.dias.profissional@gmail.com',
-    password_hash: '123',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
