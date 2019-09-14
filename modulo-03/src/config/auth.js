cfa52ab19893b12df244698444a3b15bd1f76ad2
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-09-14 11:44:43 -0300

    modulo-03

---
+++ modulo-03/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'acac9db9d0f5f1855fcdd968ec2f49d7',
+  expiresIn: '1d',
+};
--- modulo-03/src/routes/index.js
+++ modulo-03/src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import UserController from '../app/controllers/UserController';
+import SessionController from '../app/controllers/SessionController';
 
 const routes = Router();
 
 routes.post('/users', UserController.store);
+routes.post('/session', SessionController.store);
 
 export default routes;
