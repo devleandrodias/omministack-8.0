d64b32fab67557570d03dbc7b5483f10cec4d688
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-09-16 19:35:27 -0300

    modulo-03

--- modulo-03/src/routes/index.js
+++ modulo-03/src/routes/index.js
@@ -1,4 +1,7 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from '../config/multer';
 
 import UserController from '../app/controllers/UserController';
 import SessionController from '../app/controllers/SessionController';
@@ -6,9 +9,14 @@ import SessionController from '../app/controllers/SessionController';
 import authMiddleware from '../app/middlewares/auth';
 
 const routes = Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 routes.use(authMiddleware);
 
