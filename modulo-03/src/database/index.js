19cadbc66e8b04fa7ec9c0e1598dd0ea0766c640
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-09-16 21:35:28 -0300

    modulo-03

--- modulo-03/src/database/index.js
+++ modulo-03/src/database/index.js
@@ -3,8 +3,9 @@ import databaseConfig from '../config/database';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointments from '../app/models/Appointments';
 
-const models = [User, File];
+const models = [User, File, Appointments];
 
 class Database {
   constructor() {
