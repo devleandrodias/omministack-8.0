016fec293edffb5ed154b1a9c5823a1d0555a3ef
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-09-16 22:19:13 -0300

    modulo-03

---
+++ modulo-03/src/app/controllers/AppointmentsController.js
@@ -0,0 +1,63 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+import Appointments from '../models/Appointments';
+import User from '../models/User';
+
+class AppointmentsController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res
+        .status(400)
+        .json({ error: 'You can only create appointments with providers ' });
+    }
+
+    /**
+     * Check for past dates
+     */
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(date, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    /**
+     * Check date availability
+     */
+
+    const checkAvailability = Appointments.findOne({
+      where: { provider_id, canceled_at: null, date: hourStart },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not availabe' });
+    }
+
+    const appointment = await Appointments.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentsController();
--- modulo-03/src/routes/index.js
+++ modulo-03/src/routes/index.js
@@ -7,6 +7,7 @@ import FileController from '../app/controllers/FileController';
 import UserController from '../app/controllers/UserController';
 import SessionController from '../app/controllers/SessionController';
 import ProviderController from '../app/controllers/ProviderController';
+import AppointmentsController from '../app/controllers/AppointmentsController';
 
 import authMiddleware from '../app/middlewares/auth';
 
@@ -19,8 +20,11 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentsController.store);
 
 export default routes;
