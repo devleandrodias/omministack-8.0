9aaf5628ce4ecc1dfec59d3fd9fe1fc18ae7dd4a
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-09-16 22:45:10 -0300

    modulo-03

--- modulo-03/src/app/controllers/AppointmentsController.js
+++ modulo-03/src/app/controllers/AppointmentsController.js
@@ -6,9 +6,13 @@ import File from '../models/File';
 
 class AppointmentsController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointments.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       attributes: ['id', 'date'],
       include: [
         {
