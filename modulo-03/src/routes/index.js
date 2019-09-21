62ac09571b889f8ed6624801d03e62c991e38608
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-09-21 11:36:39 -0300

    modeulo-03

--- modulo-03/src/routes/index.js
+++ modulo-03/src/routes/index.js
@@ -8,6 +8,7 @@ import UserController from '../app/controllers/UserController';
 import SessionController from '../app/controllers/SessionController';
 import ProviderController from '../app/controllers/ProviderController';
 import AppointmentsController from '../app/controllers/AppointmentsController';
+import AvailableController from '../app/controllers/AvailableController';
 
 import authMiddleware from '../app/middlewares/auth';
 import ScheduleController from '../app/controllers/ScheduleController';
@@ -26,6 +27,7 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentsController.index);
 routes.post('/appointments', AppointmentsController.store);
