5515aa3acddf41606173cde8b51265200398e632
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-10-02 20:34:23 -0300

    modulo-03

--- modulo-03/src/app/controllers/UserController.js
+++ modulo-03/src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -68,9 +69,17 @@ class UserController {
       res.status(401).json({ error: 'Password not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
-    return res.json({ id, name, email, provider });
+    return res.json({ id, name, email, avatar });
   }
 }
 
