fd7f363cd57671d27e53acc192d845123fd67189
Leandro Dias <homeoffice@MacBook-Air-de-Leandro.local>
2019-10-05 08:36:58 -0300

    modulo-10

--- modulo-10/src/App.js
+++ modulo-10/src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import './configs/ReactotronConfig';
 
 import history from './services/history';
 import Routes from './routes';
 
+import store from './store';
+
 import GlobalStyles from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
-      <GlobalStyles />
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <GlobalStyles />
+        <Routes />
+      </Router>
+    </Provider>
   );
 }
 
